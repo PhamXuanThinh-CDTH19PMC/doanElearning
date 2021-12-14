ef460dfdafb5c68ec714298ad6743bc49720e159
PhamXuanThinh-CDTH19PMC <phamxuanthinhStudent@gamil.com>
2021-12-14 12:24:46 +0700

    chung thuc + template index

---
+++ DoAnElearning/app/Http/Controllers/TaiKhoanController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\TaiKhoan;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class TaiKhoanController extends Controller
+{
+    public function dangNhap(){
+        return view('dang-nhap');
+    }
+    public function xuLyDangNhap(Request $request){
+       if(Auth::attempt(['username'=>$request->ten_tai_khoan,'password'=>$request->mat_khau])){
+           $user=Auth::user();
+           return redirect()->route('index');
+        }
+        else{
+          echo "Đăng nhập không thành công";
+        }
+    } 
+    public function dangXuat(){
+        Auth::logout();
+        return redirect()->route('dang-nhap');
+    }
+}
